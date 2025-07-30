0747335bfb710dff9928cfdccb76b65a2810a674
vlaz <vlavlaz2008@gmail.com>
2025-07-30 18:42:28 +0300

    Corrected _3

--- ege/samplers/_3/_24.py
+++ ege/samplers/_3/_24.py
@@ -1,29 +1,21 @@
 with open("_24.txt") as f:
     s = f.readline()
-that_s = ""
+mx = 0
 for i in range(0, len(s)):
     if s[i] in "123":
         wei = 2
+        cnt = 1
     else:
         wei = -1
-    sub_s = s[i]
+        cnt = 0
     for j in range(i+1, len(s)):
         if s[j] in "123":
-            wei += 2
+            wei+=2
+            cnt+=1
         else:
-            wei -= 1
-        sub_s+=s[j]
+            wei-=1
         if wei == 0:
-            break
-    if wei == 0 and len(that_s) < len(sub_s):
-        print(that_s)
-        that_s = sub_s
-print(that_s, len(that_s))
-cntd = 0
-cntl = 0
-for c in that_s:
-    if c in "123":
-        cntd += 1
-    else:
-        cntl += 1
-print(cntl, cntd)
+            if mx < cnt:
+                mx = cnt
+                print(cnt)
+print(mx*3)
